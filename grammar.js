56f145211de9b52ca601d40fc008707b268754df
Antonio Nikishaev <me@lelf.lu>
2019-12-16 02:46:26 +0400

    [+] now we can parse trivial things

--- grammar.js
+++ grammar.js
@@ -2,17 +2,21 @@
 
 module.exports=grammar({name:'k',rules:{
 
- punct:$=>/[\s;\n]+/,N:$=>/[\dxyzλπøØ∞]+/,
- V:$=>/[:+\-*%&|<>=~!,^#_?@.$]:?/,A:$=>/[\/\\']:?/,
-
- source_file: $ => repeat($.e),
- e:           $ => choice(seq($.n,$.v,$.e),seq($.t,$.e),$.punct),
+ source_file: $ => seq($.e,repeat(seq($._sep,$.e)),optional($._sep)),
+ e:           $ => prec.right(choice(prec(1,seq($.n,$.v,$.e)),seq($.t,$.e),$.t)),
  t:           $ => choice($.n,$.v),
  v:           $ => choice(seq($.t,$.A),$.V),
- n:           $ => seq($.t,choice($.brkE,$.prnE,$.crlE,$.N)),
- E:           $ => choice(repeat1($.E),$.e),
- brkE:        $ => seq('[',$.E,']'),
+ n:           $ => choice(seq($.t,$.brkE),$.prnE,$.crlE,$.N),
+ E:           $ => choice(seq($.E,$.e),$.e),
  prnE:        $ => seq('(',$.E,')'),
+ brkE:        $ => seq('[',$.E,']'),
  crlE:        $ => seq('{',$.E,'}'),
 
-}})//:~
+ N:$=> choice($.int, /[xyzλπøØ∞]+/), int:$=>/\d+/,
+
+ _sep:$=>/[\n;]/,
+ V:$=>/[:+\-*%&|<>=~!,^#_?@.$]:?/,
+ A:$=>/[\/\\\']:?/,
+
+}
+})//:~
