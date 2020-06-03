d682212088556cb9da5ee1aa5d47a784fa511ea4
Antonio Nikishaev <me@lelf.lu>
2020-06-04 02:58:35 +0400

    [~] simplify intv

--- grammar.js
+++ grammar.js
@@ -5,7 +5,7 @@ module.exports=grammar({name:'k',rules:{k:$=>$._e,
 _e:$=>prec.dynamic(-1,choice($.ass,$.dap,$.map,$.cap,$._t,$.exp)),
       ass:$=>prec.dynamic(1,seq($._n,optional($.v),':',$._e)),
       exp:$=>seq(':',$._e),
-      dap:$=>prec.dynamic(1,seq(field('a',$._n),field('v',$._v),field('b',$._e))),
+      dap:$=>prec.dynamic(1,seq(field('a',$._n),field('v',$._v),optional($._sp),field('b',$._e))),
       map:$=>seq(field('f',$._t),optional($._sp),field('a',$._e)),
       cap:$=>prec.dynamic(1,seq(choice($.cap,$._t),$._v)),
 _v:$=>choice($.avd,$.v), avd:$=>seq(field('f',$._t),field('a',$.a)),
@@ -21,7 +21,7 @@ seq: $=>seq($._e,repeat(seq($._semi,$._e))),
 lam: $=>seq('{[',field('v',optional($.args)),']',field('b',optional($.seq)),'}'),
 
 n:   $=>choice($.int1,$.intv,$.flt1,$.var),
-int1:$=>seq(optional('-'),$._pint1), _pint1:$=>/\d+/, intv:$=>seq($.int1,$._sp,repeat(seq($.int1,$._sp)),$.int1),
+int1:$=>seq(optional('-'),$._pint1), _pint1:$=>/\d+/, intv:$=>seq(repeat1(seq($.int1,$._sp)),$.int1),
 flt1:$=>seq(optional('-'),$._pflt1), _pflt1:$=>/(\d+\.|\d*\.\d+)(e-?\d+)?/,
 
 v:$=>choice('-',/[+*%!&|<>=~,^#_$?@.]/), _sp:$=>' ',
@@ -30,5 +30,6 @@ a:$=>/[\/\\\']:?/,
 var: $=>/[a-z][a-z0-9]*/, _semi:$=>/[;\n]/
 
 },conflicts:$=>[[$.dap,$._t],[$.parn,$.seq],[$.cap,$._t],[$.ass,$.dap,$._t],[$.ass,$._v],[$.dap,$.map],
-                [$.int1,$.v],[$.flt1,$.v],[$.n,$.intv],[$.intv]]
+                [$.int1,$.v],[$.flt1,$.v],[$.n,$.intv],[$.intv]],
+  //extras:$=>[]
 })
