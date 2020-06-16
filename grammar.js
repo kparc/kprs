2e7000f2197fa8468b686d07440027f2964a9395
Antonio Nikishaev <me@lelf.lu>
2020-06-16 23:18:17 +0400

    [~] rework all partial apps

--- grammar.js
+++ grammar.js
@@ -1,13 +1,20 @@
 /* e:nve|te|t..v|t t:n|v v:tA|V n:t[E*]|(E)|(E*)|{[..]E}|N
      dap map cap           avd    ap   parn list   lam     */
-module.exports=grammar({name:'k',rules:{k:$=>$._e,
+module.exports=grammar({name:'k',rules:{k:$=>choice($._v,$._e,$._pe),
 
-_e:$=>prec.dynamic(-1,choice($.ass,$.dap,$.map,$.cap,$._t,$.exp)),
-      ass:$=>prec.dynamic(1,seq(field('v',$._n),field('f',optional($.v)),':',field('a',optional($._e)))),
-      exp:$=>seq(':',$._e),
-      dap:$=>prec.dynamic(1,seq(field('a',$._n),optional($._sp),field('v',$._v),optional($._sp),field('b',$._e))),
-      map:$=>seq(field('f',$._t),optional($._sp),field('a',$._e)),
-      cap:$=>prec.dynamic(1,seq(choice(field('ca',$.cap),field('ta',$._t)),field('b',$._v))),
+_pe:$=>choice($.pdap,$.pmap),
+   pdap:$=>prec.dynamic(1,seq(field('a',$._n),optional($._sp),field('v',$._v),
+                                              optional($._sp),optional(field('z',$._pe)))),
+   pmap:$=>choice(field('v',$.v),seq(field('f',$._t),optional($._sp),field('z',choice($._v,$._pe)))),
+
+_e:$=>prec.dynamic(-1,choice($.ass,$.dap,$.map,$._t,$.exp)),
+   ass:$=>prec.dynamic(1,seq(field('v',$._n),field('f',optional($.v)),':',field('a',optional($._e)))),
+   exp:$=>seq(':',$._e),
+   dap:$=>prec.dynamic(1,seq(field('a',$._n),optional($._sp),field('v',$._v),
+                                             optional($._sp),field('b',$._e))),
+   map:$=>seq(field('f',$._t),optional($._sp),field('a',$._e)),
+   cap:$=>prec.dynamic(1,seq(choice(field('ca',$.cap),field('ta',$._t)),field('b',$._v))),
+
 _v:$=>choice($.avd,$.v), avd:$=>seq(field('f',$._t),field('a',$.a)),
 _t:$=>choice($._n,$._v),
 _n:$=>choice($.ap,$.parn,$.list,$.n,$.lam),
@@ -32,6 +39,7 @@ a:$=>/[\/\\\']:?/,
 var: $=>/[a-z][a-z0-9]*/, _semi:$=>/[;\n]/
 
 },conflicts:$=>[[$.dap,$._t],[$.parn,$.seq],[$.cap,$._t],[$.ass,$.dap,$._t],[$.ass,$._v],[$.dap,$.map],
-                [$.int1,$.v],[$.flt1,$.v],[$.n,$.intv],[$.intv],[$.intv,$.v]],
+                [$.int1,$.v],[$.flt1,$.v],[$.n,$.intv],[$.intv],[$.intv,$.v],
+                [$.pmap,$._t],[$.ass,$.dap,$.pdap,$._t],[$.dap,$.pdap,$._t],[$.pmap,$._v],[$.k,$._t]],
   extras:$=>[]
 })
