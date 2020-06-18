19751934b28f8d7c0b6f85e724524127d1dbf218
Antonio Nikishaev <me@lelf.lu>
2020-06-18 23:00:50 +0400

    [+] less boilerplate

--- grammar.js
+++ grammar.js
@@ -1,40 +1,39 @@
 /* e:nve|te|t..v|t t:n|v v:tA|V n:t[E*]|(E)|(E*)|{[..]E}|N
      dap map cap           avd    ap   parn list   lam     */
-module.exports=grammar({name:'k',rules:{k:$=>seq(repeat($._ksep),$._k,repeat(seq($._ksep,optional($._k)))),
-                                       _k:$=>choice($._v,$._e,$._pe),
+F=field; O=optional; C=choice; R=repeat; R1=repeat1; S=seq; D=prec.dynamic; D1=x=>D(1,x);
+module.exports=grammar({name:'k',rules:{k:$=>S(R($._ksep),$._k,R(S($._ksep,O($._k)))),
+                                       _k:$=>C($._v,$._e,$._pe),
 
-_pe:$=>choice($.pdap,$.pmap),
-   pdap:$=>prec.dynamic(1,seq(field('a',$._n),optional($._sp),field('v',$._v),
-                                              optional($._sp),optional(field('z',$._pe)))),
-   pmap:$=>choice(field('v',$.v),seq(field('f',$._t),optional($._sp),field('z',choice($._v,$._pe)))),
+_pe:$=>C($.pdap,$.pmap),
+   pdap:$=>D1(S(F('a',$._n),O($._sp),F('v',$._v),O($._sp),O(F('z',$._pe)))),
+   pmap:$=>   C(F('v',$.v),S(F('f',$._t),O($._sp),F('z',C($._v,$._pe)))),
 
-_e:$=>prec.dynamic(-1,choice($.ass,$.dap,$.map,$._t,$.exp)),
-   ass:$=>prec.dynamic(1,seq(field('v',$._n),field('f',optional($.v)),':',field('a',optional($._e)))),
-   exp:$=>seq(':',$._e),
-   dap:$=>prec.dynamic(1,seq(field('a',$._n),optional($._sp),field('v',$._v),
-                                             optional($._sp),field('b',$._e))),
-   map:$=>seq(field('f',$._t),optional($._sp),field('a',$._e)),
-   cap:$=>prec.dynamic(1,seq(choice(field('ca',$.cap),field('ta',$._t)),field('b',$._v))),
+_e:$=>D(-1,C($.ass,$.dap,$.map,$._t,$.exp)),
+   ass:$=>D1(S(F('v',$._n),F('f',O($.v)),':',F('a',O($._e)))),
+   exp:$=>   S(':',$._e),
+   dap:$=>D1(S(F('a',$._n),O($._sp),F('v',$._v),O($._sp),F('b',$._e))),
+   map:$=>   S(F('f',$._t),O($._sp),F('a',$._e)),
+   cap:$=>D1(S(C(F('ca',$.cap),F('ta',$._t)),F('b',$._v))),
 
-_v:$=>choice($.avd,$.v), avd:$=>seq(field('f',$._t),field('a',$.a)),
-_t:$=>choice($._n,$._v),
-_n:$=>choice($.ap,$.parn,$.list,$.n,$.lam),
+_v:$=>C($.avd,$.v), avd:$=>S(F('f',$._t),F('a',$.a)),
+_t:$=>C($._n,$._v),
+_n:$=>C($.ap,$.parn,$.list,$.n,$.lam),
 
-parn:$=>seq('(',$._e,')'),
-list:$=>seq('(',optional($.seq),')'),
-ap:  $=>prec(1,seq(field('f',$._e),'[',field('a',optional($.seq)),']')),
+parn:$=>S('(',$._e,')'),
+list:$=>S('(',O($.seq),')'),
+ap:  $=>prec(1,S(F('f',$._e),'[',F('a',O($.seq)),']')),
 
-args:$=>seq($.var,repeat(seq(';',$.var))),
-seq: $=>choice(repeat1($._semi),seq(repeat($._semi),seq($._e,repeat(seq($._semi,optional($._e)))))),
-lam: $=>seq('{[',field('v',optional($.args)),']',field('b',optional($.seq)),'}'),
+args:$=>S($.var,R(S(';',$.var))),
+seq: $=>C(R1($._semi),S(R($._semi),S($._e,R(S($._semi,O($._e)))))),
+lam: $=>S('{[',F('v',O($.args)),']',F('b',O($.seq)),'}'),
 
-n:   $=>choice($.int1,$.intv,$.flt1,$.var),
+n:   $=>C($.int1,$.intv,$.flt1,$.var),
 
-int1:$=>seq(optional('-'),$._pint1), _pint1:$=>/\d+/,
-intv:$=>seq(optional('-'),$._pintv), _pintv:$=>/\d+( -?\d+)+/,
-flt1:$=>seq(optional('-'),$._pflt1), _pflt1:$=>/(\d+\.|\d*\.\d+)(e-?\d+)?/,
+int1:$=>S(O('-'),$._pint1), _pint1:$=>/\d+/,
+intv:$=>S(O('-'),$._pintv), _pintv:$=>/\d+( -?\d+)+/,
+flt1:$=>S(O('-'),$._pflt1), _pflt1:$=>/(\d+\.|\d*\.\d+)(e-?\d+)?/,
 
-v:$=>choice('-',/[+*%!&|<>=~,^#_$?@.]/), _sp:$=>' ',
+v:$=>C('-',/[+*%!&|<>=~,^#_$?@.]/), _sp:$=>' ',
 a:$=>/[\/\\\']:?/,
 
 var: $=>/[a-zA-Z][a-zA-Z0-9]*/, _semi:$=>/;|\n\s+/, _ksep:$=>/;|\n/
