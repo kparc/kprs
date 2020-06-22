ddbbdf6662cc8d08f3ffb7a4f85a3cc50c91aabd
Antonio Nikishaev <me@lelf.lu>
2020-06-22 21:54:10 +0400

    [~] _e _pe supertypes

--- grammar.js
+++ grammar.js
@@ -43,5 +43,5 @@ var: $=>/[a-zA-Z][a-zA-Z0-9]*/, _semi:$=>C(/;\s*/,S(O($.nb),/\n\s+/)), _ksep:$=>
 
 },conflicts:$=>[[$.pmap,$.pdap,$._e],[$.pass,$.ass],[$.pmap,$._e],[$.pmap,$.pdap],[$.dap,$.map],
                 [$.pmap,$._k,$._e],[$.parn,$.seq]],
-  externals:$=>[$._ugh],  inline:$=>[$._t],  extras:$=>[]
+  externals:$=>[$._ugh],  inline:$=>[$._t],  supertypes:$=>[$._e,$._pe],  extras:$=>[]
 })
