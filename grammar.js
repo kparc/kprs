8da229a82552fccdf6fe58d32f1304029517eb2b
Antonio Nikishaev <me@lelf.lu>
2020-06-22 23:02:26 +0400

    [~] n→lit v→op

--- grammar.js
+++ grammar.js
@@ -5,22 +5,22 @@ module.exports=grammar({name:'k',rules:{k:$=>S(R($._ksep),F('k',$._k),O($.nb),R(
                                        _k:$=>C(D(1,$._v),$._e,$._pe),
 
 _pe:$=>D(-1,C($.pass,$.pdap,$.pdam,$.pmap,$.pexp)),
-   pass:$=>D(1,P(1,S(F('v',$._n),F('f',O($.v)),':',F('a',O($._pe))))),
-   pexp:$=>                                  S(':',        $._pe),
-   pdap:$=>D(2,S(F('a',$._n),O($._sp),F('v',$._v),         O($._sp),O(F('z',$._pe)))),
-   pdam:$=>D(2,S(F('a',$._n),         F('v',A($._ugh,$.v)),O($._sp),O(F('z',$._pe)))),
-   pmap:$=>D(1,C(F('v',$._v),       S(F('f',$._t),         O($._sp),F('z',$._pe)))),
+   pass:$=>D(1,P(1,S(F('v',$._n),F('f',O($.op)),':',F('a',O($._pe))))),
+   pexp:$=>                                   S(':',        $._pe),
+   pdap:$=>D(2,S(F('a',$._n),O($._sp),F('v',$._v),          O($._sp),O(F('z',$._pe)))),
+   pdam:$=>D(2,S(F('a',$._n),         F('v',A($._ugh,$.op)),O($._sp),O(F('z',$._pe)))),
+   pmap:$=>D(1,C(F('v',$._v),       S(F('f',$._t),          O($._sp),F('z',$._pe)))),
 
 _e:$=>D(-1,C($.ass,$.dap,$.dam,$.map,$._t,$.exp)),
-   ass:$=>D(1,P(1,S(F('v',$._n),F('f',O($.v)),':',F('a',O($._e))))),
-   exp:$=>                                  S(':',        $._e),
-   dap:$=>D(2,S(F('a',$._n),O($._sp),F('v',$._v),         O($._sp),F('b',$._e))),
-   dam:$=>D(2,S(F('a',$._n),         F('v',A($._ugh,$.v)),O($._sp),F('b',$._e))),
+   ass:$=>D(1,P(1,S(F('v',$._n),F('f',O($.op)),':',F('a',O($._e))))),
+   exp:$=>                                  S(':',         $._e),
+   dap:$=>D(2,S(F('a',$._n),O($._sp),F('v',$._v),          O($._sp),F('b',$._e))),
+   dam:$=>D(2,S(F('a',$._n),         F('v',A($._ugh,$.op)),O($._sp),F('b',$._e))),
    map:$=>D(1,S(F('f',$._t),O($._sp),F('a',$._e))),
 
-_v:$=>C($.avd,$.v), avd:$=>S(F('f',$._t),F('a',$.a)),
+_v:$=>C($.avd,$.op), avd:$=>S(F('f',$._t),F('a',$.a)),
 _t:$=>C($._n,$._v),
-_n:$=>C($.ap,$.parn,$.list,$.n,$.lam),
+_n:$=>C($.ap,$.parn,$.list,$.lit,$.lam),
 
 parn:$=>S('(',$._k,')'),
 list:$=>S('(',O($.seq),')'),
@@ -30,13 +30,13 @@ args:$=>S($.var,R(S(';',$.var))),
 seq: $=>C(R1($._semi),S(R($._semi),S($._k,R(S($._semi,O($._k)))))),
 lam: $=>S('{[',F('v',O($.args)),']',F('b',O($.seq)),'}'),
 
-n:   $=>C($.int1,$.intv,$.flt1,$.fltv,$.sym1,$.symv,$.chr1,$.chrv,$.var), //move everything to lexer
+lit: $=>C($.int1,$.intv,$.flt1,$.fltv,$.sym1,$.symv,$.chr1,$.chrv,$.var), //move everything to lexer
 int1:$=>/-?\d+/,                        intv:$=>/-?\d+( -?\d+)+/,
 flt1:$=>/-?(\d+\.|\d*\.\d+)(e-?\d+)?/,  fltv:$=>/-?(\d+\.?|\d*\.?\d+)(e-?\d+)?( -?(\d+\.?|\d*\.\d+)(e-?\d+)?)+/,
 sym1:$=>/`[\w.]*/,                      symv:$=>/(`[\w.]*)+/,
 chr1:$=>/"([^\"]|\\.)"/,                chrv:$=>C(/""/,/"([^\"]|\\.)+"/),
 
-v:$=>C('-',/[+*%!&|<>=~,^#_$?@.]/), _sp:$=>' ', nb:$=>/\s+\/[^\n]+/,
+op:$=>C('-',/[+*%!&|<>=~,^#_$?@.]/), _sp:$=>' ', nb:$=>/\s+\/[^\n]+/,
 a:$=>/[\/\\\']:?/,
 
 var: $=>/[a-zA-Z][a-zA-Z0-9]*/, _semi:$=>C(/;\s*/,S(O($.nb),/\n\s+/)), _ksep:$=>C(/;\s*/,/\n/),
